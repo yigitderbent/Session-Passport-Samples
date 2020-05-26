8e476bb38bfd9469811e4532474832bd9abcfdff
Yigit Derbent <yigitderbent@gmail.com>
2020-05-26 07:43:14 +0200

    Session Timeout and Session Destroy

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const secret = process.env.SECRET;
 app.use(session({
   secret: secret,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie :{maxAge:10000}
 }));
 
 // middleware request which will be executed for every path
@@ -21,6 +22,11 @@ app.use((req, res, next) =>{
 app.get("/",(req,res)=>{
     console.log("Seesion ID  ",req.session.id);
     res.send("You viewed this page " + req.session.views +  " times");
+});
+
+app.get("/logout",(req,res)=>{
+    req.session.destroy();
+    res.send("Session killed");
 });
 
 app.listen(process.env.PORT || 3000, _=>{
